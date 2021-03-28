7852798b339a3e99b5c9f78857900b6242417d49
Dmytro Mamenko <mamenko.dv@gmail.com>
2021-03-28 20:18:57 +0200

    feat: findoutlier  duplicatencode  cata

--- codewarsCata.js
+++ codewarsCata.js
@@ -98,33 +98,33 @@
 
 // returns "Caught!" because the cat can catch the mouse in 4 moves
 
-function catMouse(map, moves) {
-  if (!map.includes("m") || !map.includes("C")) {
-    return "boring without two animals";
-  }
-  let lines = map.split("\n");
-  catLine = lines.filter((string) => {
-    return string.indexOf("C") >= 0;
-  });
-  mouseLine = lines.filter((string) => {
-    return string.indexOf("m") >= 0;
-  });
-  catPosition = +catLine.toString().indexOf("C");
+// function catMouse(map, moves) {
+//   if (!map.includes("m") || !map.includes("C")) {
+//     return "boring without two animals";
+//   }
+//   let lines = map.split("\n");
+//   catLine = lines.filter((string) => {
+//     return string.indexOf("C") >= 0;
+//   });
+//   mouseLine = lines.filter((string) => {
+//     return string.indexOf("m") >= 0;
+//   });
+//   catPosition = +catLine.toString().indexOf("C");
 
-  mousePosition = +mouseLine.toString().indexOf("m");
+//   mousePosition = +mouseLine.toString().indexOf("m");
 
-  let mouseLineNumber = +lines.indexOf(mouseLine.toString());
-  let catLineNumber = +lines.indexOf(catLine.toString());
-  return Math.abs(catPosition - mousePosition) +
-    Math.abs(catLineNumber - mouseLineNumber) <=
-    moves
-    ? "Caught!"
-    : "Escaped!";
-}
-const ree = `..C......
-m........
-.........`;
-console.log(catMouse(ree, 6));
+//   let mouseLineNumber = +lines.indexOf(mouseLine.toString());
+//   let catLineNumber = +lines.indexOf(catLine.toString());
+//   return Math.abs(catPosition - mousePosition) +
+//     Math.abs(catLineNumber - mouseLineNumber) <=
+//     moves
+//  ..   ? "Caught!"
+//     : "Escaped!";
+// }
+// const ree = `..C......
+// m........
+// .........`;
+// console.log(catMouse(ree, 6));
 
 // const FilterString = function (value) {
 //   return +value.match(/\d/g).join("");
@@ -199,3 +199,26 @@ console.log(catMouse(ree, 6));
 //   validPass("ThisPasswordIsTooLong1234"),
 //   "ThisPasswordIsTooLong1234"
 //);
+
+// You are given an array (which will have a length of at least 3, but could be very large)
+//  containing integers. The array is either entirely comprised of odd integers or entirely
+// comprised of even integers except for a single integer N. Write a method that takes the
+// array as an argument and returns this "outlier" N.
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+// Should return: 11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21]
+// Should return: 160 (the only even number)
+function findOutlier(integers) {
+  let allEven = integers.filter((item) => item % 2 === 0);
+  let allOdd = integers.filter((item) => item % 2 !== 0);
+
+  return allEven.length === 1 ? allEven[0] : allOdd[0];
+  // if (!allEven) {
+  //   return integers.filter((item) => item % 2 !== 0);
+  // } else {
+  //   return integers.filter((item) => item % 2 === 0);
+  // }
+}
+let integer = [2, 4, 0, 100, 4, 11, 2602, 36];
+console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]));
